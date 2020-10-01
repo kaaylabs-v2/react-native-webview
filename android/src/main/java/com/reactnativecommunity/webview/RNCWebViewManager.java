a6518de303bfe6ae2f9a5fe11e7c15ec7c92da7f
selvan <muthamil.selvan@kaaylabs.com>
2020-10-01 22:00:42 +0530

    fix

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -805,7 +805,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
         final WritableMap event = createWebViewEvent(view, request.getUrl().toString());
         event.putInt("lockIdentifier", lockIdentifier);
         rncWebView.sendDirectMessage("onShouldStartLoadWithRequest", event);
-        System.out.println('from shouldInterceptRequest' + request.getUrl().toString());
+        System.out.println("from shouldInterceptRequest" + request.getUrl().toString());
         return null;
      }
 
